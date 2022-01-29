808619a24a8fc4a4fac700031f297316be3fcab2
darnelld <darnelldugger@gmail.com>
2022-01-28 19:51:04 -0500

    start on task 2

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
+  getGuess: function(guessNum){
+    let numGuess = ()
+    
+  }`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`,
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
 }
 
+console.log(game)
